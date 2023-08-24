af3383c23e0c524a81ab18e4aed646fdb5ec6674
steven tringali <dr.tringali@gmail.com>
2023-08-23 20:57:31 -0700

    burrito bowl

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,8 @@
             <div class="flex justify-center px-0 mt-16 sm:items-center sm:justify-between">
                 <div class="text-sm text-center text-gray-500 sm:text-left">
                     <div class="flex items-center gap-4">
-                        <a href="https://www.buymeacoffee.com/tringali" target="_blank">Buy me a pizza</a>
+                        <a href="https://www.buymeacoffee.com/tringali" target="_blank">Buy me a burrito bowl
+                            &#127791; &#129379; </a>
                     </div>
                 </div>
                 <div class="ml-4 text-sm text-center text-gray-500 sm:text-right sm:ml-0">
