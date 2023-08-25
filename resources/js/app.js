cf1781a44cc037f0130b2c2924d2a57b8d7dfbee
Steven Tringali <dr.tringali@gmail.com>
2023-08-24 23:05:41 -0700

    added sentry for issues and performance monitoring

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
 import './bootstrap';
 import Alpine from 'alpinejs'
 import focus from '@alpinejs/focus'
+import * as Sentry from "@sentry/browser";
 
+Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
+});
 Alpine.plugin(focus)
 window.Alpine = Alpine
 Alpine.start()
