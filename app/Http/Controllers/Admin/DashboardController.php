4c94636dfa7d4bf2e7868d4aa265a1acd9039bba
Steven Tringali <dr.tringali@gmail.com>
2023-08-26 00:25:52 -0700

    add unrecognied and unparsable labs

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Lab;
 use App\Models\Panel;
+use App\Models\UnparsableLab;
+use App\Models\UnrecognizedLab;
 
 class DashboardController extends Controller
 {
@@ -16,7 +18,14 @@ class DashboardController extends Controller
             ->orderBy('panels.order_column')
             ->orderBy('labs.order_column')
             ->get(['labs.label', 'panels.label as panel']);
+        $unparsableLabs = UnparsableLab::all();
+        $unrecognizedLabs = UnrecognizedLab::all();
 
-        return view('admin.dashboard', compact('panels', 'labs'));
+        return view('admin.dashboard', compact(
+            'panels',
+            'labs',
+            'unparsableLabs',
+            'unrecognizedLabs',
+        ));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,6 +9,34 @@
             Dashboard</h1>
         <div class=" grid mb-8 border border-gray-200 rounded-lg shadow-sm md:mb-12 md:grid-cols-2 items-start">
             <div class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
+                <h3 class="max-w-2xl mx-auto mb-2 text-lg font-semibold text-gray-900">Unparsable Labs</h3>
+                <ul class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg ">
+                    @forelse($unparsableLabs as $lab)
+                        <li class="w-full px-4 py-2 border-b border-gray-200 last:border-0 first:rounded-t-lg last:rounded-b-lg">
+                            {{ $lab->name }}
+                        </li>
+                    @empty
+                        <li class="w-full px-4 py-2">
+                            All clear!
+                        </li>
+                    @endforelse
+                </ul>
+            </div>
+            <div class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-tr-lg dark:bg-gray-800 dark:border-gray-700">
+                <h3 class="max-w-2xl mx-auto mb-2 text-lg font-semibold text-gray-900">Unrecognized Labs</h3>
+                <ul class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg ">
+                    @forelse($unrecognizedLabs as $lab)
+                        <li class="w-full px-4 py-2 border-b border-gray-200 last:border-0 first:rounded-t-lg last:rounded-b-lg">
+                            {{ $lab->name }}
+                        </li>
+                    @empty
+                        <li class="w-full px-4 py-2">
+                            All clear!
+                        </li>
+                    @endforelse
+                </ul>
+            </div>
+            <div class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
                 <h3 class="max-w-2xl mx-auto mb-2 text-lg font-semibold text-gray-900">Panels</h3>
                 <ul class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg ">
                     @foreach($panels as $panel)
