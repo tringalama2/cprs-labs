8ee5f073ecfd50f024aab99ffd5e164a6f7bf377
Steven Tringali <dr.tringali@gmail.com>
2023-09-02 00:18:09 -0700

    flash messages alert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.base')
 
 @section('body')
+    <x-flash/>
     @yield('content')
 
     @isset($slot)
         {{ $slot }}
     @endisset
-    
+
     @livewire('livewire-ui-modal')
 @endsection
