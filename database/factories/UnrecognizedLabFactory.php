cfbb7991419c1a5c3c671ad3ee970b1b2491f589
Steven Tringali <dr.tringali@gmail.com>
2023-09-08 22:53:26 -0700

    test all admin routes

---
+++ database/factories/UnrecognizedLabFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\UnrecognizedLab;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UnrecognizedLabFactory extends Factory
+{
+    protected $model = UnrecognizedLab::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+        ];
+    }
+}
--- tests/Feature/AdminTest.php
+++ tests/Feature/AdminTest.php
@@ -1,7 +1,26 @@
 <?php
 
-it('restricts dashboard to admin users', function () {
+use App\Models\Panel;
+use App\Models\UnrecognizedLab;
+
+it('restricts access to admin users', function (string $route, string $method) {
     login(null, ['is_admin' => false])
-        ->get(route('admin.dashboard'))
+        ->$method($route)
         ->assertForbidden();
-});
+})->with([
+    'admin.dashboard' => [fn () => route('admin.dashboard'), 'get'],
+    'admin.panel.index' => [fn () => route('admin.panel.index'), 'get'],
+    'admin.panel.create' => [fn () => route('admin.panel.create'), 'get'],
+    'admin.panel.store' => [fn () => route('admin.panel.store'), 'post'],
+    'admin.panel.show' => [fn () => route('admin.panel.show', Panel::factory()->create()), 'get'],
+    'admin.panel.edit' => [fn () => route('admin.panel.edit', Panel::factory()->create()), 'get'],
+    'admin.panel.update' => [fn () => route('admin.panel.update', Panel::factory()->create()), 'put'],
+    'admin.unprocessed-labs.edit' => [
+        fn () => route('admin.unprocessed-labs.edit',
+            UnrecognizedLab::factory()->create()), 'get',
+    ],
+    'admin.unprocessed-labs.update' => [
+        fn () => route('admin.unprocessed-labs.update',
+            UnrecognizedLab::factory()->create()), 'post',
+    ],
+]);
