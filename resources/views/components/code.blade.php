8f51c374e59f361aa5d8bf7138845b1e394dc210
steven tringali <dr.tringali@gmail.com>
2024-04-20 15:19:15 -0700

    improved code padding

--- resources/views/components/code.blade.php
+++ resources/views/components/code.blade.php
@@ -1,1 +1,1 @@
-<code {{ $attributes->class(['tracking-widest text-xs text-left bg-gray-700 text-white rounded-md p-1.5 me-1']) }}>{{ $slot->isEmpty() ? $text : $slot }}</code>
+<code {{ $attributes->class(['tracking-widest text-xs text-left bg-gray-700 text-white rounded-md py-1.5 px-2 me-1']) }}>{{ $slot->isEmpty() ? $text : $slot }}</code>
