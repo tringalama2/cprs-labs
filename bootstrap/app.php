31bb1c429f69536e3ecca8dabd79b788884fb7c1
Steven Tringali <dr.tringali@gmail.com>
2024-05-08 22:22:08 -0700

    ,

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'isAdmin' => isAdmin::class
+            'isAdmin' => isAdmin::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
