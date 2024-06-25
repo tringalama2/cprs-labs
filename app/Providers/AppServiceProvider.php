2f818a9cf785cce9fe89f72c60f65461b274ea2f
Steven Tringali <dr.tringali@gmail.com>
2024-06-25 02:32:36 -0700

    strict models

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Model::shouldBeStrict(! $this->app->isProduction());
+
         Blade::if('admin', function () {
             return auth()->user()?->is_admin;
         });
