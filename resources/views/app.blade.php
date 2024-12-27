9d0e51cee26d1aa42626f0bad908351fc3f9691e
Steven Tringali <dr.tringali@gmail.com>
2024-12-27 01:37:27 -0800

    modal component

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,7 +19,7 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @stack('styles')
     <style>
         body {
