eefc5cd83fc15527ade1551eb6098bd94a83e2d4
Steven Tringali <dr.tringali@gmail.com>
2025-02-06 00:47:09 -0800

    tailwind class extraction for less browser memory

--- resources/js/Components/LabResults.vue
+++ resources/js/Components/LabResults.vue
@@ -132,21 +132,21 @@ onMounted(function () {
             <table class="text-sm border-collapse border-spacing-0">
                 <thead>
                 <tr style="font-weight: bolder">
-                    <th class="text-center border-b border-gray-500 px-2 z-20 sticky bg-gray-200 top-0" scope="col"></th>
-                    <th class="text-center border-b border-gray-500 px-2 z-20 sticky bg-gray-200 top-0" scope="col"></th>
-                    <th v-for="specimen in dateTimeHeaders.value" class="text-center border-b border-gray-500 px-2 z-20 sticky bg-gray-200 top-0"
+                    <th class="dateHeader" scope="col"></th>
+                    <th class="dateHeader" scope="col"></th>
+                    <th v-for="specimen in dateTimeHeaders.value" class="dateHeader"
                         scope="col"
                         v-html="specimen.collectionDate.toFormat('L/d/yy<br />H:mm')"></th>
                 </tr>
                 </thead>
                 <tbody>
                 <template v-for="(label, key, index) in labels.value">
-                    <tr class="border-b border-gray-500 hover:bg-sky-200 group">
+                    <tr class="resultRow group">
                         <th v-if="labels.value[Object.keys(labels.value)[index]]?.panel!==labels.value[Object.keys(labels.value)[index-1]]?.panel"
-                            :rowspan="panels.value[label.panel]" class="font-extrabold border-r border-gray-500 group-hover:bg-white text-xl text-start ps-3 z-30 sticky bg-white left-0"
+                            :rowspan="panels.value[label.panel]" class="panelHeader"
                             style="writing-mode: vertical-lr;">{{ label.panel }}
                         </th>
-                        <th class="border-r border-gray-500 px-2 bg-gray-200 group-hover:bg-sky-300 z-20 sticky left-7" scope="row"
+                        <th class="labelHeader" scope="row"
                         >{{ label.label }}
                         </th>
                         <ResultCell v-for="specimen in dateTimeHeaders.value"
--- resources/js/Components/ResultCell.vue
+++ resources/js/Components/ResultCell.vue
@@ -15,15 +15,14 @@ const props = defineProps({
 const critical = props.flag?.includes('*');
 const flag = props.flag?.match(/^([LH])$/) && !critical;
 const classObject = computed(() => ({
-    'bg-red-500 text-red-950 group-hover:bg-sky-500 font-bold': critical,
-    'bg-red-300 text-red-900 group-hover:bg-sky-400 font-bold': flag,
-    'group-hover:bg-sky-200 bg-white': !(critical || flag),
+    'criticalResult': critical,
+    'abnormalResult': flag,
+    'normalResult': !(critical || flag),
 }))
 </script>
 
 <template>
-    <td :class="classObject"
-        class="border-r border-gray-500 px-2 text-center whitespace-nowrap">{{ result }}
+    <td :class="classObject">{{ result }}
     </td>
 </template>
 
